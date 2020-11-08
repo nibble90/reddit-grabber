81d49aaf0f5f234bb0b0bcbb1e5807c17d83e039
nibble90 <18497547+nibble90@users.noreply.github.com>
2020-11-08 21:20:37 +0000

    Adding post_cache table

--- main_classes.py
+++ main_classes.py
@@ -28,6 +28,7 @@ class database:
     def __init__(self, db="/home/ubuntu/jacobbot/reddit-grabber/posts.db"):
         self.db = db
         self.__create_databases()
+        self.uuids = []
 
     def __create_databases(self):
         connection = sqlite3.connect(self.db)
@@ -48,7 +49,7 @@ class database:
     def unix_time(self):
         return int(time.time())
 
-    def write_pics(self, subreddit=None, title=None, score=None, url=None, selftext=None, author=None, post_id=None):
+    def write_cache(self, subreddit=None, title=None, score=None, url=None, selftext=None, author=None, post_id=None):
         connection = sqlite3.connect(self.db)
         c = connection.cursor()
         title = str(title, )
@@ -62,10 +63,49 @@ class database:
             (subreddit, title, score, url, selftext, author, post_id))
         connection.commit()
         connection.close()
+    
+    def write_timestamps(self, post0=None, post1=None, post2=None, post3=None, post4=None, post5=None, post6=None, post7=None, post8=None, post9=None):
+        connection = sqlite3.connect(self.db)
+        c = connection.cursor()
+        unix_timestamp = self.unix_time()
+        post0 = str(post0, )
+        post1 = str(post1, )
+        post2 = str(post2, )
+        post3 = str(post3, )
+        post4 = str(post4, )
+        post5 = str(post5, )
+        post6 = str(post6, )
+        post7 = str(post7, )
+        post8 = str(post8, )
+        post9 = str(post9, )
+        c.execute("INSERT INTO posts_cache(unix_timestamp, post0, post1, post2, post3, post4, post5, post6, post7 post8, post9) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+            (unix_timestamp, post0, post1, post2, post3, post4, post5, post6, post7, post8, post9))
+        connection.commit()
+        connection.close()
+
+    def grab_uuid(self, post_id=None):
+        connection = sqlite3.connect(self.db)
+        c = connection.cursor()
+        post_id = str(post_id, )
+        c.execute("SELECT uuid FROM posts WHERE id=?", (post_id, ))
+        result = c.fetchall()
+        connection.commit()
+        connection.close()
+        return (result[0][0])
+
+    def uuid_info(self, post_id):
+        uuid = self.grab_uuid(post_id)
+        self.uuids.append(uuid)
+
+    def cache_into_timestamps(self):
+        self.write_timestamps(self.uuids[0], self.uuids[1], self.uuids[2], self.uuids[3], self.uuids[4], self.uuids[5], self.uuids[6], self.uuids[7], self.uuids[8], self.uuids[9])
 
 
 if __name__ == "__main__":
     pics = RedditAPI().pics()
     db = database()
     for title, score, url, selftext, author, post_id in pics:
-        db.write_pics("pics", title, score, url, selftext, author, post_id)
+        db.write_cache("pics", title, score, url, selftext, author, post_id)
+        db.uuid_info(post_id)
+    db.cache_into_timestamps()
+
